0eed8f52e102a4e6f80aba466f97f52e9eedf386
mkallgren08 <mkallgren08@gmail.com>
2018-08-05 21:48:57 -0400

    Figured out how to access session storage in order to pass user data around

---
+++ mysql_commands/seeds/skills/base_skills.sql
@@ -0,0 +1,7 @@
+-- skillid(auto increment), name, att, candefault, bookref, pageref, skilltype, 
+
+INSERT INTO SkillBases (name,att,candefault,pageref,skilltype)
+-- Archery
+VALUES (NULL, 'Archery','Agl',1,'Core Rules', 130,'Active'),
+-- Automatics
+(NULL, 'Automatics','Agl',1,'Core Rules',130,'Active');
